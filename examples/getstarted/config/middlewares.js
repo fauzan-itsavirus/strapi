ea77af5cede8445b140d206ff5e6d8a4c19bca55
Ahmad Fauzan <akisearu17@Ahmads-MacBook-Air.local>
2023-03-06 13:19:06 +0800

    enable cors and add dockerfile

--- examples/getstarted/config/middlewares.js
+++ examples/getstarted/config/middlewares.js
@@ -5,8 +5,15 @@ const responseHandlers = require('./src/response-handlers');
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['*']
+    }
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
